6f15cefb3164dc4a008749c91b43fd68357b30c7
James Rickelton <jjarickelton@gmail.com>
2021-04-06 14:51:14 +0100

    ✨ Created Nav component

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>topic 1</li>
+          <li>topic 2</li>
+          <li>topic 3</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
